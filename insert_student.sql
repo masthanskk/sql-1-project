da881c80cc71d6ec90f2bbd174592e60dc63ceff
shami <skmasthan.9919@gmail.com>
2022-12-03 15:06:26 +0530

    sql project up set up

---
+++ insert_student.sql
@@ -0,0 +1,6 @@
+INSERT INTO student VALUES
+(1,'SYED','10TH',56,'male'),
+(2,'shami','9th', 66,'male'),
+(3,'varun','8th',68,'Male'),
+(4,'asifa','9th',77,'female'),
+(5,'Harini','10th',87,'female'); 
---
+++ students.sql
@@ -0,0 +1,8 @@
+CREATE TABLE "student" (
+	"Id"	INTEGER,
+	"Name"	TEXT,
+	"Class"	TEXT,
+	"Mark"	INTEGER NOT NULL,
+    "Gender" TEXT NOT NULL,
+	PRIMARY KEY("Id" AUTOINCREMENT)
+);
